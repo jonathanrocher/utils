5cd3606efa45672edfd050080a0dec1d090fec0a
Jonathan Rocher <jrocher@enthought.com>
2011-08-01 17:43:40 -0500

    Reducing verbosity

--- multi_search_replace.py
+++ multi_search_replace.py
@@ -20,7 +20,7 @@ def search_for_files(folder_path, extension_list = [".py"],
         If safe == True, a copy of the folder is created and both are kept
     """
 
-    print("Now dealing with folder %s" % os.path.abspath(folder_path))
+    print("Now exploring with folder %s" % os.path.abspath(folder_path))
     content = os.listdir(folder_path)
     os.chdir(folder_path)
 
@@ -28,21 +28,19 @@ def search_for_files(folder_path, extension_list = [".py"],
     eligible_files = []
 
     for member in content:
-        print("   Now dealing with %s" % os.path.abspath(member))
         if os.path.isfile(member):
             if file_to_apply(member, extension_list, 
                              exclude_file_list):
-                print("      Adding %s to the list of eligible files" 
+                print("      Adding %s to the eligible files" 
                       % member)
                 eligible_files.append(os.path.abspath(member))
-                print eligible_files
         elif os.path.isdir(member):
            eligible_files += search_for_files(member, 
                                               extension_list, 
                                               exclude_file_list)
         else:
-            print("   Skipping %s." % os.path.abspath(member))
-
+            #print("   Skipping %s." % os.path.abspath(member))
+            pass
     return eligible_files
 
 
@@ -52,7 +50,7 @@ def file_to_apply(filename, extension_list, exclude_file_list):
 
         FIXME: make smarter filters
     """
-    print("      testing %s" % filename)
+    #print("      testing %s" % filename)
     condition = (os.path.splitext(filename)[1] in extension_list
                  and filename not in exclude_file_list)
     
@@ -68,7 +66,7 @@ def find_and_replace(filepath, new_filepath, string1, string2):
     """
 
     num_occurences = 0
-    print("Replacing '%s' by '%s' in %s ") % (string1,string2,filepath)
+    #print("Replacing '%s' by '%s' in %s ") % (string1,string2,filepath)
     try:
         f = open(filepath, "r")
     except Exception as e:
@@ -79,13 +77,13 @@ def find_and_replace(filepath, new_filepath, string1, string2):
         num_occurences += 1
         offset = content.find(string1)
         new_content = content[:offset]+string2+content[offset+len(string1):]
-        import pdb ; pdb.set_trace()
         content = new_content
-    print "*** Found %s occurences of %s. ***" % (num_occurences, string1)
     f.close()
     
     if num_occurences:
-        print "Saving to ", new_filepath
+        print("*** Found %s occurences of %s in %s. ***" % 
+              (num_occurences, string1, filepath))
+        #print "Saving to ", new_filepath
         f = open(new_filepath, "w")
         f.write(content)
         f.close()
@@ -104,11 +102,9 @@ def main(folder_path, string1, string2 = "", extension_list = [".py"],
                                       exclude_file_list = [])
     
     # FIXME: create the tree structure automatically and recursively
-    print "eligible_files", eligible_files
 
     for files in eligible_files:
         if safe:
-            print "files", files
             target_filename = files.replace(containing_folder, 
                                             containing_folder + postfix)
             if not os.path.exists(os.path.dirname(target_filename)):
